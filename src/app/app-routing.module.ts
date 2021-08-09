579e5182141df88e575dfc60e384f730e5713d71
Leonardo <leofonseca.magalhaes@gmail.com>
2021-08-09 15:10:22 -0300

    projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { ProductCreateComponent } from './componets/product/product-create/product-create.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent }  from './viwes/home/home.component';
+import { ProductCrudComponent } from './viwes/product-crud/product-crud.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:"",
+  component: HomeComponent
+},{
+  path: "products",
+  component: ProductCrudComponent
+},{ 
+  path:"products/create",
+  component: ProductCreateComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './componets/template/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { FooterComponent } from './componets/template/footer/footer.component';
+import { NavComponent } from './componets/template/nav/nav.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from'@angular/material/list';
+import { HomeComponent } from './viwes/home/home.component';
+import { ProductCrudComponent } from './viwes/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
+import { ProductCreateComponent } from './componets/product/product-create/product-create.component';
+import { MatButtonModule } from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    ProductCrudComponent,
+    RedDirective,
+    ForDirective,
+    ProductCreateComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
